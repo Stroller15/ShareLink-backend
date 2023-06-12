2c36794b438aca5c38ea7e8634dabc098d07953d
Your Name <shubhamverma6351@gmail.com>
2023-06-13 01:55:27 +0530

    initial commit

---
+++ controller/image-controller.js
@@ -0,0 +1,38 @@
+import File from '../models/file.js';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const BASE_URL = process.env.BASE_URL || "6010";
+
+export const uploadImage = async (request, response) => {
+    const fileObj = {
+        path: request.file.path,
+        name: request.file.originalname,
+    }
+    
+    try {
+        const file = await File.create(fileObj);
+        response.status(200).json({ path: `${BASE_URL}/file/${file._id}` });
+    } catch (error) {
+        console.error(error.message);
+        response.status(500).json({ error: error.message });
+    }
+}
+
+export const getImage = async (request, response) => {
+    try {   
+        const file = await File.findById(request.params.fileId);
+        
+        file.downloadCount++;
+
+        await file.save();
+
+        response.download(file.path, file.name);
+    } catch (error) {
+        console.error(error.message);
+        response.status(500).json({ msg: error.message });
+    }
+}
